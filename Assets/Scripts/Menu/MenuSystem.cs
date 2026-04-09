052b2b1867d1da51f93c51ae514b8fb9dd86d2d2
h8moss <danarmenta32@gmail.com>
2026-04-09 17:59:56 -0600

    Implemented dynamic level laoding in play

--- Assets/Scripts/Menu/MenuSystem.cs
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -9,7 +9,11 @@ public class MenuSystem : MonoBehaviour
   
     public void Play()
     {
-        SceneManager.LoadScene(2);
+        int maxLevel = 4; // TODO: Change this to the real max level? Figure out a better implementation
+        int levelToLoad = Mathf.Min(maxLevel, SaveManager.Instance.State.LevelProgress+1);
+
+        // We add one because scene 0 is the main menu and scene 1 is the level menu, so level 1 is scene 2.
+        SceneManager.LoadScene(levelToLoad + 1);
     }
 
     public void SelectLevels()
