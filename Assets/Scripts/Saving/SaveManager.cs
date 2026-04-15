1c46e897d6363b13b32e50f46446bc75791221f0
h8moss <danarmenta32@gmail.com>
2026-04-14 18:12:51 -0600

    Fixed saving for web builds

--- Assets/Scripts/Saving/SaveManager.cs
+++ Assets/Scripts/Saving/SaveManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SaveManager
 {
     private static SaveManager instance = null;
+    private const string PlayerPrefsKey = "SaveData";
 
     public static SaveManager Instance{ 
         get {
@@ -21,7 +22,42 @@ public class SaveManager
 
     public SaveState State { get; private set; }
 
+private bool IsWebBuild()
+    {
+#if UNITY_WEBGL
+        return true;
+#else
+        return false;
+#endif
+    }
+
     private void LoadData()
+    {
+        if (IsWebBuild())
+        {
+            LoadDataFromPlayerPrefs();
+        }
+        else
+        {
+            LoadDataFromFile();
+        }
+    }
+
+    private void SaveData()
+    {
+        if (IsWebBuild())
+        {
+            SaveDataToPlayerPrefs();
+        }
+        else
+        {
+            SaveDataToFile();
+        }
+    }
+
+
+
+    private void LoadDataFromFile()
     {
         string path = Application.persistentDataPath + "/save.json";
         if (File.Exists(path))
@@ -33,12 +69,34 @@ public class SaveManager
             State = SaveState.Defaults();
         }
     }
-    private void SaveData()
+    private void SaveDataToFile()
     {
         string json = JsonUtility.ToJson(State, true);
         string path = Application.persistentDataPath + "/save.json";
         File.WriteAllText(path, json);
     }
+
+    private void LoadDataFromPlayerPrefs()
+    {
+        if (PlayerPrefs.HasKey(PlayerPrefsKey))
+        {
+            string json = PlayerPrefs.GetString(PlayerPrefsKey);
+            State = JsonUtility.FromJson<SaveState>(json);
+        }
+        else
+        {
+            State = SaveState.Defaults();
+        }
+
+    }
+
+    private void SaveDataToPlayerPrefs()
+    {
+        string json = JsonUtility.ToJson(State, false);
+        PlayerPrefs.SetString(PlayerPrefsKey, json);
+        PlayerPrefs.Save();
+    }
+
 
     public void Save(SaveState newData)
     {
