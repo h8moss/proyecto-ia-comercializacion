255e52cae45bfc9cab79811987d5cae5a178a5b2
Arbolillo <Arbolillogaymer@gmail.com>
2026-05-19 00:59:01 -0600

    Revert "add WASD tutorial popup at level 1 start"
    This reverts commit 61262f4e787b4d8b6dc88ae855e71ce3263c3ebf.

---
+++ Assets/Scripts/UI/WASDKeyAnimation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WASDKeyAnimation : MonoBehaviour
+{
+    [Header("Teclas")]
+    [SerializeField] private Image keyW;
+    [SerializeField] private Image keyA;
+    [SerializeField] private Image keyS;
+    [SerializeField] private Image keyD;
+
+    [Header("Colores")]
+    [SerializeField] private Color normalColor = new Color(1f, 1f, 1f, 1f);
+    [SerializeField] private Color pressedColor = new Color(1f, 0.77f, 0.09f, 1f); // amarillo
+
+    [Header("Tiempos")]
+    [SerializeField] private float pressDuration = 0.3f;
+    [SerializeField] private float delayBetween = 0.2f;
+
+    void OnEnable()
+    {
+        StartCoroutine(AnimateKeys());
+    }
+
+    IEnumerator AnimateKeys()
+    {
+        while (true)
+        {
+            yield return PressKey(keyW);
+            yield return new WaitForSeconds(delayBetween);
+            yield return PressKey(keyA);
+            yield return new WaitForSeconds(delayBetween);
+            yield return PressKey(keyS);
+            yield return new WaitForSeconds(delayBetween);
+            yield return PressKey(keyD);
+            yield return new WaitForSeconds(delayBetween * 2);
+        }
+    }
+
+    IEnumerator PressKey(Image key)
+    {
+        if (key == null) yield break;
+        key.color = pressedColor;
+        yield return new WaitForSeconds(pressDuration);
+        key.color = normalColor;
+    }
+}
